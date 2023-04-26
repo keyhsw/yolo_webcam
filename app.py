5952329817939b473007eb15132b0c3a320a2512
key <30455795+keyhsw@users.noreply.github.com>
2023-04-26 18:13:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 import tensorflow as tf
+import os
 import cv2
 import numpy as np
 from glob import glob
 from models import Yolov4
 import gradio as gr
+
+# Download the checkpoints
+os.system('wget -P /home/xlab-app-center/ https://huggingface.co/spaces/jbraun19/Webcam-Object-Recognition-Yolo-n-Coco/resolve/main/yolov4.weights')
+
 model = Yolov4(weight_path="yolov4.weights", class_name_path='coco_classes.txt')
 def gradio_wrapper(img):
     global model
